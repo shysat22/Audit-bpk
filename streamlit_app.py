017fb2c2e66fa65fc80ef60961d60c5ddcbba819
shysat22 <sa.tyasmoro@gmail.com>
2026-04-18 15:11:46 +0900

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.markdown("""
     </style>
     <div class="main-header">
         <h2 style='margin:0;'>🏛️ Sistem Sampling Digital</h2>
-        <p style='margin:0; font-size: 0.9em;'>Pemeriksaan Kepatuhan - BPK RI</p>
+        <p style='margin:0; font-size: 0.9em;'>Metode Stratified MPU</p>
     </div>
     """, unsafe_allow_html=True)
 
